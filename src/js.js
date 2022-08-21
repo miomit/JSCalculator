96f0f3207a1f9ff97bcdd8cadecf571aff74cb54
Yura Movsesyan <movsesyan-03@inbox.ru>
2022-08-21 22:21:08 +0300

    update

--- src/js.js
+++ src/js.js
@@ -23,33 +23,87 @@ const solve = (tree) => {
     }
 }
 
-let lexer = (() => {
-
-    const initOperation = (symbol, func) => {
-        return {
-            symbol: symbol,
-            func: func
-        }
-    }
-
-    let binOperations = [
-        initOperation('+', (a, b) => a + b),
-        initOperation('-', (a, b) => a - b),
-        initOperation('*', (a, b) => a * b),
-        initOperation('+', (a, b) => a + b),
-        initOperation('^', (a, b) => a ** b)
-    ]
-     
+const initOperation = (symbol, func) => {
     return {
-        getOpr: () => {
-            return binOperations
-        }
+        symbol: symbol,
+        func: func
     }
-})();
+}
 
-let a = Tree(2, (a, b) => a + b, Tree(0, 1), Tree(0, 5))
+const binOperations = [
+    initOperation('+', (a, b) => a + b),
+    initOperation('-', (a, b) => a - b),
+    initOperation('*', (a, b) => a * b),
+    initOperation('+', (a, b) => a + b),
+    initOperation('^', (a, b) => a ** b)
+]
 
-let a2 = Tree(2, (a, b) => a + b, Tree(1, (a) => a*a, Tree(0, 2)), Tree(0, 0))
+const unoOperations = [
+    initOperation('sqrt', (a) => Math.sqrt(a)),
+    initOperation('sin', (a) => Math.sin(a)),
+    initOperation('cos', (a) => Math.cos(a))
+]
 
-print(solve(a), lexer.getOpr()[4].func(2, 3))
+const findFirsCloseBracket = (index = 0, bracketsCount = 0, str) => {
+    if (str.length == index) return -1
+    else if (str[index] == '(') return findFirsCloseBracket(index + 1, bracketsCount + 1, str)
+    else if (str[index] == ')' && bracketsCount > 0) return findFirsCloseBracket(index + 1, bracketsCount - 1, str)
+    else if (bracketsCount > 0) return findFirsCloseBracket(index + 1, bracketsCount, str)
+    else if (str[index] == ')') return index
+    else return findFirsCloseBracket(index + 1, bracketsCount, str)
+}
 
+const closeBracketId = (str) => findFirsCloseBracket(0, 0, str)
+
+const openingBracket = (str) => str.slice(1, closeBracketId(str))
+
+const findByValue = (index, bracketsCount, str, value) =>{
+    if (str.length == index) return -1
+    else if (str[index] == '(') return findByValue(index + 1, bracketsCount + 1, str, value)
+    else if (str[index] == ')') return findByValue(index + 1, bracketsCount - 1, str, value)
+    else if (bracketsCount > 0) return findByValue(index + 1, bracketsCount, str, value)
+    else if (str[index] == value) return index
+    else return findByValue(index + 1, bracketsCount, str, value)
+}
+
+const findByVal = (str, val) => findByValue (0, 0, str, val)
+
+const findOperation = (index, str) =>{
+    if (binOperations.length == index) return [false]
+    const id = findByVal(str, binOperations[index].symbol)
+    
+    if (id > -1) return [true, id, binOperations[index].func]
+    else return findOperation(index + 1, str)
+}
+
+const findOpr = (str) => findOperation(0, str)
+
+const findFunctionByName = (index, name) => {
+    if (unoOperations.length == index) return [false]
+    if (unoOperations[index].symbol == name) return [true, unoOperations[index].func]
+    else return findFunctionByName(index + 1, name)
+}
+
+const findFuncByName = (name) => findFunctionByName(0, name)
+
+let findFunction = (index, str) => {
+    if (str.length == index || (str[0] >= '0' && str[0] <= '9')) return [false]
+    else if ((str[0] >= '0' && str[0] <= '9') || str[index] == '(' || str[index] == '-'){ 
+        let [isFunc, fn] = findFuncByName(str.slice(0, index))
+        return [isFunc, index, fn]
+    }
+    else return findFunction (index + 1, str)
+}
+
+let findFunc = (str) => findFunction(0, str)
+
+const lexer = (str) => {
+    const [isOpr, id, binFinc] = findOpr(str)
+
+    if (isOpr) return Tree(2, binFinc, lexer(str.slice(0, id)), lexer(str.slice(id+1)))
+    else if (str[0] == '(') return lexer(openingBracket(str)) 
+    
+    const [isFunc, length, unoFunc] = findFunc(str)
+    if (isFunc) return Tree(1, unoFunc, lexer(str.slice(length)))
+    else return Tree(0, Number(str))
+}
